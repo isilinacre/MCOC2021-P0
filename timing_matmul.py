dd5ba2106b15a47ab547b90977f43fdaeabe1d47
isilinacre <iplinacre@miuandes.cl>
2021-08-02 18:20:25 -0400

    Update timing_matmul.py
    Ejercicio hecho en clases 2.08

--- timing_matmul.py
+++ timing_matmul.py
@@ -1,20 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 from numpy import zeros
 from time import perf_counter
 
-
-
-#Tamaño
 N = 1000
-A = zeros((N, N))+1
-B = zeros((N, N))+2
+A = zeros((N,N)) +1  
+B = zeros((N,N)) +2
 
+print(f"A={A}")
+print(f"B={B}")
+
+C = A@B
 
 t1 = perf_counter()
-C = A@B
 t2 = perf_counter()
-
 
 dt = t2 - t1
 
-print(f"dt = {dt} s")
+print(f"dt={dt} s")
 
